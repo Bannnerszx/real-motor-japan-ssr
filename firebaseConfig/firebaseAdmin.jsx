e665722b92e04f0b38b6691a62eb756759ae25c5
Bannnerszx <github-action-713721121@samplermj.iam.gserviceaccount.com>
2025-02-07 11:39:39 +0900

    2nd commit ver4.5

--- firebaseConfig/firebaseAdmin.jsx
+++ firebaseConfig/firebaseAdmin.jsx
@@ -2,8 +2,9 @@ import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
     admin.initializeApp({
-      credential: admin.credential.cert(require('../service-account-key.json'))
+      credential: admin.credential.cert(serviceAccount)
     });
   } catch (error) {
     console.error('Firebase admin initialization error:', error);
