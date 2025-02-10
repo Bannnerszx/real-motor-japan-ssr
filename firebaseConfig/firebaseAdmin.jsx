96a0de5f26a9f54954922d7210555c202b04528f
Bannnerszx <github-action-713721121@samplermj.iam.gserviceaccount.com>
2025-02-10 12:37:45 +0900

    2nd commit ver5.6.3

--- firebaseConfig/firebaseAdmin.jsx
+++ firebaseConfig/firebaseAdmin.jsx
@@ -1,17 +1,9 @@
 import * as admin from 'firebase-admin';
 
 
-if (!admin.apps.length) {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-  } catch (error) {
-    console.error('Firebase admin initialization error:', error);
-  }
-}
 
 const db = admin.firestore();
 const storage = admin.storage();
 
 export { db, storage };
+
