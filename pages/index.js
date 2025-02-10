d2befc4582b613d3e70191807df3829025417a2f
Bannnerszx <github-action-713721121@samplermj.iam.gserviceaccount.com>
2025-02-10 16:17:30 +0900

    2nd commit ver5.6.8

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,8 @@ import SEOBrandList from '../SEOListings/SEOBrandList';
 import SEOTypeList from '../SEOListings/SEOTypeList';
 import StickyFooter from '../homeComponents/StickyFooter';
 import OptimizeCarousel from '../homeComponents/OptimizeCarousel';
-
+import { db } from '../lib/firebaseAdmin';
 export async function getServerSideProps() {
-  const { db } = await import('../lib/firebaseAdmin');
 
   try {
 
